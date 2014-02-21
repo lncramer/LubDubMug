3f27ff4c5c463a82f5e47d6191bb96f440da8774
Jacob Lee <jacobtwlee@gmail.com>
2014-02-21 01:07:47 -0800

    Character Animations and Rotation
    Yay.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ public class PlayerController : MonoBehaviour {
 
 	// Player fields
 	float move_force = 20f;
-	
+	float speed; // velocity vector magnitude
+	public Animator animator;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,7 +15,17 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// TODO: Change key events to be handled with the input manager so player can remap keys in game
+
+		// Update animator speed parameter
+		speed = rigidbody2D.velocity.SqrMagnitude ();
+		animator.SetFloat ("speed", speed);
+
+		// Update direction
+		if (speed > 0)
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, rigidbody2D.velocity);
+
+
+		// TODO: Change key events to be handled with the input manager (using KeyCode) so player can remap keys in game
 		if (Input.GetKey("up")) {
 			rigidbody2D.AddForce(new Vector2(0f, move_force));
 		}
